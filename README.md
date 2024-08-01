b27f692937b6b1858689f68707196b953b6b8587
Hesam Zarnooshe <javad.zarnooshe@gmail.com>
2024-08-01 17:42:00 +0330

    web build added

--- README.md
+++
@@ -1,2 +0,0 @@
-# weatherApp
-WeatherApp with Flutter framework
--- android/app/src/main/kotlin/com/weatherapp/hesamzs/weatherapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.weatherapp.hesamzs.weatherapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
